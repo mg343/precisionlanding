693bdbe0d6a4d32bb634533d6af8e4f3201bb7dc
mihir garimella <mihir.g@rotor.ai>
2025-06-25 14:32:37 -0400

    corresponding cpp file

---
+++ aruco_utils.cpp
@@ -0,0 +1,13 @@
+#include "perc/aruco_utils.hpp"
+
+// project
+#include "math/utils.hpp"
+
+// external
+#include <spdlog/spdlog.h>
+
+namespace perc {
+
+//dont think anything is needed
+
+}  // namespace perc
